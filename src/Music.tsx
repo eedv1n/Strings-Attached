970c31b8bc11f2cf76b14a2e01c701014e342d1b
ItsDevWasTaken <edvinduartemartins11@gmail.com>
2024-10-03 16:44:55 +0200

    Lade till en title till musiksidan

--- src/Music.tsx
+++ src/Music.tsx
@@ -3,6 +3,7 @@ import * as Elements from './Elements';
 function Music() {
 	return (
 		<Elements.AnimMain>
+			<h1 className="title">Musik</h1>
 			<Elements.AudioPlayer src="https://cdn.pixabay.com/audio/2024/09/29/audio_9a75aacce7.mp3" />
 			<Elements.AudioPlayer src="https://cdn.pixabay.com/audio/2024/09/29/audio_5c67567261.mp3" />
 		</Elements.AnimMain>
